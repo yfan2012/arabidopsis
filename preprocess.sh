619e382a7a6e166b9c3e86baf0f6ee878375dddf
yfan2012 <yfan2012@gmail.com>
2020-11-19 12:56:01 -0500

    preprocessing for 201106_arabidopsis run

---
+++ preprocess.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+prefix=201106_arabidopsis
+datadir=/dilithium/Data/Nanopore/projects/arabadopsis/$prefix
+
+
+ref=/mithril/Data/NGS/Reference/arab/TAIR10.fa
+refmmi=/mithril/Data/NGS/Reference/arab/TAIR10.mmi
+
+
+if [ $1 == fastqs ] ; then
+    ##gather all the fastq files into on
+    mkdir -p $datadir/fqs
+    
+    cat $datadir/raw/fastq_pass/*fastq > $datadir/fqs/$prefix.fq
+fi
+
+fq=$datadir/fqs/$prefix.fq
+
+if [ $1 == align ] ; then
+    ##align the reads using minimap2
+    mkdir -p $datadir/align
+
+    minimap2 -ax map-ont -t 36 $refmmi $fq |
+	samtools view -@ 36 -b |
+	samtools sort -@ 36 -o $datadir/align/$prefix.sorted.bam
+    samtools index $datadir/align/$prefix.sorted.bam
+fi
+
+
